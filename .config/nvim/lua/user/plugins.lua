5f115a58b41531d4e8b4674a3c5f0e9904f48912
Stanislav Spatari <spatari.stanislav@gmail.com>
2022-04-17 14:56:41 +0300

    feat(nvim): update colorscheme

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -48,10 +48,7 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim" -- Easily comment stuff
 
   -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  -- use "lunarvim/darkplus.nvim"
-  -- use "ellisonleao/gruvbox.nvim"
-  use 'folke/tokyonight.nvim'
+  use "lunarvim/darkplus.nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
