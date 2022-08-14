7b64379a3b4b6aae8cb9d989cee06e2ed17dd98f
Stanislav Spatari <spatari.stanislav@gmail.com>
2022-08-14 22:41:35 +0300

    feat(nvim): set default colorsheme to tokyonight to support undercurl

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,4 +1,10 @@
-local colorscheme = "gruvbox"
+local colorscheme = "tokyonight"
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
