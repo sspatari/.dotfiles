7802e9205d124888dc5351c69021465bb9f6507f
Stanislav Spatari <spatari.stanislav@gmail.com>
2022-09-16 10:53:04 +0300

    Revert "fix(tsserver-lsp): opening react/index.d.ts file in quick fix when going to some definitions in react"
    This reverts commit d1e31de7318a085b19970fc82ca0a5a8099b821a.

--- nvim/.config/nvim/lua/user/lsp/typescript.lua
+++ nvim/.config/nvim/lua/user/lsp/typescript.lua
@@ -13,35 +13,6 @@ local on_attach = function(client)
   ts_utils.setup_client(client)
 end
 
-local function filter(arr, fn)
-  if type(arr) ~= "table" then
-    return arr
-  end
-
-  local filtered = {}
-  for k, v in pairs(arr) do
-    if fn(v, k, arr) then
-      table.insert(filtered, v)
-    end
-  end
-
-  return filtered
-end
-
-local function filterReactDTS(value)
-  return string.match(value.uri, "react/index.d.ts") == nil
-end
-
 require("lspconfig").tsserver.setup({
   on_attach = on_attach,
-  handlers = {
-    ["textDocument/definition"] = function(err, result, method, ...)
-      if vim.tbl_islist(result) and #result > 1 then
-        local filtered_result = filter(result, filterReactDTS)
-        return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
-      end
-
-      vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
-    end,
-  },
 })
