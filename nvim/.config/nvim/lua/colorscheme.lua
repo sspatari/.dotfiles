05a6d9475b1487728b152e7e3e1e8bd18ef68417
Stanislav Spatari <spatari.stanislav@gmail.com>
2023-09-08 19:35:42 +0300

    fix(nvim-tree): tree not showing after update and dim inactive

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -1,5 +1,14 @@
 local colorscheme = "catppuccin"
 
+require("catppuccin").setup({
+  show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+  dim_inactive = {
+    enabled = true, -- dims the background color of inactive window
+    shade = "dark",
+    percentage = 0.01, -- percentage of the shade to apply to the inactive window
+  },
+})
+
 local status_ok, _ = pcall(vim.api.nvim_command, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
