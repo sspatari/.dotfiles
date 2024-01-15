4cecd9bb0844ca1a985f9e2e5dd110d3ee091262
Stanislav Spatari <spatari.stanislav@gmail.com>
2024-01-16 01:53:47 +0200

    fix: attempt to fix typescript lsp displaying multiple files when go-to-definition is called on a react component

--- nvim/.config/nvim/lua/plugins/lsp/typescript.lua
+++ nvim/.config/nvim/lua/plugins/lsp/typescript.lua
@@ -1,9 +1,14 @@
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
 local on_attach = function(client)
   client.server_capabilities.documentFormattingProvider = false
   client.server_capabilities.documentRangeFormattingProvider = false
 
-  local status_ok, ts_utils = pcall(require, "nvim-lsp-ts-utils")
-  if not status_ok then
+  local ts_utils_status_ok, ts_utils = pcall(require, "nvim-lsp-ts-utils")
+  if not ts_utils_status_ok then
     return
   end
 
@@ -13,6 +18,36 @@ local on_attach = function(client)
   ts_utils.setup_client(client)
 end
 
-require("lspconfig").tsserver.setup({
+local function filter(arr, fn)
+  if type(arr) ~= "table" then
+    return arr
+  end
+
+  local filtered = {}
+  for k, v in pairs(arr) do
+    if fn(v, k, arr) then
+      table.insert(filtered, v)
+    end
+  end
+
+  return filtered
+end
+
+local function filterReactDTS(value)
+  return string.match(value.targetUri or value.uri, "react/index.d.ts") == nil
+end
+
+lspconfig.tsserver.setup({
   on_attach = on_attach,
+  -- other options
+  handlers = {
+    ["textDocument/definition"] = function(err, result, method, ...)
+      if vim.tbl_islist(result) and #result > 1 then
+        local filtered_result = filter(result, filterReactDTS)
+        return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+      end
+
+      vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+    end,
+  },
 })
