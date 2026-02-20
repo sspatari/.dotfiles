b4141506bfe3c42ec4187bc3a2d639b01961ba37
Stanislav Spatari <spatari.stanislav@gmail.com>
2026-02-20 18:21:50 +0200

    fix(nvim): tailwind lsp not working

--- nvim/.config/nvim/lua/plugins/lsp/tailwind.lua
+++ nvim/.config/nvim/lua/plugins/lsp/tailwind.lua
@@ -1,6 +1,1 @@
-local root_pattern = require("lspconfig").util.root_pattern
-
-vim.lsp.config("tailwindcss", {
-  root_dir = root_pattern("tailwind.config.js", "tailwind.config.ts", "postcss.config.js", "postcss.config.ts"),
-})
 vim.lsp.enable("tailwindcss")
