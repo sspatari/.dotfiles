fc234c843623060ad59d6434773394bd2bf6022d
Stanislav Spatari <spatari.stanislav@gmail.com>
2026-02-23 22:38:18 +0200

    feat(nvim): simple conform config for go and remove the root .bin adding to path as go tool appeared

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,34 +1,14 @@
 return {
-  "stevearc/conform.nvim",
+  'stevearc/conform.nvim',
   config = function()
-    local ok, conform = pcall(require, "conform")
-    if not ok then
+    local status_ok, conform = pcall(require, "conform")
+    if not status_ok then
       return
-    end
-
-    local function goimports_cmd()
-      -- Find go.mod upwards from current buffer dir
-      local buf_dir = vim.fn.expand("%:p:h")
-      local gomod = vim.fn.findfile("go.mod", buf_dir .. ";")
-      if gomod ~= "" then
-        local root = vim.fn.fnamemodify(gomod, ":h")
-        local local_goimports = root .. "/.bin/goimports"
-        if vim.fn.filereadable(local_goimports) == 1 then
-          return local_goimports
-        end
-      end
-      -- fallback to global
-      return "goimports"
     end
 
     conform.setup({
       formatters_by_ft = {
         go = { "goimports", "gofmt", stop_after_first = true },
-      },
-      formatters = {
-        goimports = {
-          command = goimports_cmd,
-        },
       },
       format_on_save = {
         lsp_fallback = true,
